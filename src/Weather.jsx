76a5e65f4159eeff1ebe850c0dd4f97e1fc32da8
Mace Akkari <mace.akkari@relyfe.com>
2022-07-18 11:21:00 +0100

    add styling changes

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import d2d from "degrees-to-direction";
 import Favourites from "./Favourites";
+import { ReactComponent as Search } from "./icons/Search.svg";
 
 const favouritesFromStorage = JSON.parse(localStorage.getItem("favourites"));
 
@@ -30,20 +31,24 @@ const Weather = () => {
   return (
     <div className="weather_container">
       <h1>Find a forecast</h1>
-      <input
-        className="input_box"
-        placeholder="search for a place...."
-        onChange={(event) => setSelectedCity(event.target.value)}
-        value={selectedCity}
-        onKeyPress={weatherApi}
-      />
+      <div className="search_container">
+        <input
+          className="input_box"
+          placeholder="search for a place...."
+          onChange={(event) => setSelectedCity(event.target.value)}
+          value={selectedCity}
+          onKeyPress={weatherApi}
+        />
+        <Search />
+      </div>
       {typeof weatherData.name === "undefined" ? (
         <div></div>
       ) : (
-        <div>
+        <div className="results_container">
           <p>Location: {weatherData.name}</p>
-          <p> Wind Speed: {weatherData.wind.speed} mph</p>
+          <p>Wind speed: {weatherData.wind.speed} mph</p>
           <p>Wind direction: {d2d(weatherData.wind.deg)}</p>
+          <p>Description: {weatherData.weather[0].description} </p>
         </div>
       )}
 
